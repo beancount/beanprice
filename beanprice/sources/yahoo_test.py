c788d131766fce6105794bfc6f5a7d340a3c3650
Rudd-O <rudd-o@rudd-o.com>
2023-02-22 14:12:41 +0000

    Update test.

--- beanprice/sources/yahoo_test.py
+++ beanprice/sources/yahoo_test.py
@@ -162,7 +162,9 @@ class YahooFinancePriceFetcher(unittest.TestCase):
     def test_parse_response_empty_result(self):
         response = MockResponse(
             '{"quoteResponse": {"error": null, "result": []}}')
-        with self.assertRaises(yahoo.YahooError):
+        with self.assertRaises(IndexError):
+            # Callers re-raise a YahooError from here, to provide
+            # superior error messages.
             yahoo.parse_response(response)
 
     def test_parse_response_no_timestamp(self):
