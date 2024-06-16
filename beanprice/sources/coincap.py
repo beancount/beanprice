642252f9c77db795ff2666c6b3fd2057d14ce0a5
blais <blais@furius.ca>
2024-06-16 17:32:42 -0400

    Disabled broken tests.

--- beanprice/sources/coincap.py
+++ beanprice/sources/coincap.py
@@ -19,7 +19,7 @@ from typing import List, Optional, Tuple, Dict
 import requests
 from beanprice import source
 
-API_BASE_URL = 'https://api.coincap.io/v2/'
+API_BASE_URL = "https://api.coincap.io/v2/"
 
 
 class CoincapError(ValueError):
@@ -32,10 +32,10 @@ def get_asset_list() -> List[Dict[str, str]]:
     elements with many properties, including "id", representing the Coincap id,
     and "symbol", representing the ticker symbol.
     """
-    path = 'assets/'
+    path = "assets/"
     url = API_BASE_URL + path
     response = requests.get(url)
-    data = response.json()['data']
+    data = response.json()["data"]
     return data
 
 
@@ -46,8 +46,8 @@ def get_currency_id(currency: str) -> Optional[str]:
     """
     # Array is already sorted based on market cap
     for coin in get_asset_list():
-        if coin['symbol'] == currency:
-            return coin['id']
+        if coin["symbol"] == currency:
+            return coin["id"]
     return None
 
 
@@ -60,40 +60,45 @@ def resolve_currency_id(base_currency: str) -> str:
         # Try to find currency ID by its symbol
         base_currency_id = get_currency_id(base_currency)
         if not isinstance(base_currency_id, str):
-            raise CoincapError("Could not find currency id with ticker '"
-                               + base_currency + "'")
+            raise CoincapError(
+                "Could not find currency id with ticker '" + base_currency + "'"
+            )
         return base_currency_id
     else:
         return base_currency
 
 
 def get_latest_price(base_currency: str) -> Tuple[float, float]:
-    path = 'assets/'
+    path = "assets/"
     url = API_BASE_URL + path + resolve_currency_id(base_currency)
     response = requests.get(url)
     data = response.json()
-    timestamp = data['timestamp'] / 1000.0
-    price_float = data['data']['priceUsd']
+    timestamp = data["timestamp"] / 1000.0
+    price_float = data["data"]["priceUsd"]
     return price_float, timestamp
 
 
-def get_price_series(base_currency_id: str, time_begin: datetime.datetime,
-                     time_end: datetime.datetime) -> List[source.SourcePrice]:
-    path = 'assets/{}/history'.format(base_currency_id)
+def get_price_series(
+    base_currency_id: str, time_begin: datetime.datetime, time_end: datetime.datetime
+) -> List[source.SourcePrice]:
+    path = "assets/{}/history".format(base_currency_id)
     params = {
-        'interval': 'd1',
-        'start': str(math.floor(time_begin.timestamp() * 1000.0)),
-        'end': str(math.ceil(time_end.timestamp() * 1000.0))
+        "interval": "d1",
+        "start": str(math.floor(time_begin.timestamp() * 1000.0)),
+        "end": str(math.ceil(time_end.timestamp() * 1000.0)),
     }
     url = API_BASE_URL + path
     response = requests.get(url, params=params)
-    return [source.SourcePrice(
-        Decimal(item['priceUsd']),
-        datetime.datetime.fromtimestamp(
-            item['time'] / 1000.0).replace(tzinfo=datetime.timezone.utc),
-        'USD'
-    )
-        for item in response.json()['data']]
+    return [
+        source.SourcePrice(
+            Decimal(item["priceUsd"]),
+            datetime.datetime.fromtimestamp(item["time"] / 1000.0).replace(
+                tzinfo=datetime.timezone.utc
+            ),
+            "USD",
+        )
+        for item in response.json()["data"]
+    ]
 
 
 class Source(source.Source):
@@ -104,22 +109,25 @@ class Source(source.Source):
     def get_latest_price(self, ticker) -> source.SourcePrice:
         price_float, timestamp = get_latest_price(ticker)
         price = Decimal(price_float)
-        price_time = datetime.datetime.fromtimestamp(timestamp).\
-            replace(tzinfo=datetime.timezone.utc)
-        return source.SourcePrice(price, price_time, 'USD')
+        price_time = datetime.datetime.fromtimestamp(timestamp).replace(
+            tzinfo=datetime.timezone.utc
+        )
+        return source.SourcePrice(price, price_time, "USD")
 
-    def get_historical_price(self, ticker: str,
-                             time: datetime.datetime) -> Optional[source.SourcePrice]:
-        for datapoint in self.get_prices_series(ticker,
-                                                time +
-                                                datetime.timedelta(days=-1),
-                                                time + datetime.timedelta(days=1)):
+    def get_historical_price(
+        self, ticker: str, time: datetime.datetime
+    ) -> Optional[source.SourcePrice]:
+        for datapoint in self.get_prices_series(
+            ticker, time + datetime.timedelta(days=-1), time + datetime.timedelta(days=1)
+        ):
+            # TODO(blais): This is poorly thought out, the date may not match
+            # that in the differing timezone. You probably want the last price
+            # before the datapoint time.
             if datapoint.time is not None and datapoint.time.date() == time.date():
                 return datapoint
         return None
 
-    def get_prices_series(self, ticker: str,
-                          time_begin: datetime.datetime,
-                          time_end: datetime.datetime
-                          ) -> List[source.SourcePrice]:
+    def get_prices_series(
+        self, ticker: str, time_begin: datetime.datetime, time_end: datetime.datetime
+    ) -> List[source.SourcePrice]:
         return get_price_series(resolve_currency_id(ticker), time_begin, time_end)
--- beanprice/sources/yahoo_test.py
+++ beanprice/sources/yahoo_test.py
@@ -8,6 +8,7 @@ import unittest
 from decimal import Decimal
 from unittest import mock
 
+import pytest
 from dateutil import tz
 import requests
 
@@ -27,9 +28,9 @@ class MockResponse:
 
 
 class YahooFinancePriceFetcher(unittest.TestCase):
-
     def _test_get_latest_price(self):
-        response = MockResponse(textwrap.dedent("""
+        response = MockResponse(
+            textwrap.dedent("""
             {"quoteResponse":
              {"error": null,
               "result": [{"esgPopulated": false,
@@ -48,23 +49,27 @@ class YahooFinancePriceFetcher(unittest.TestCase):
                           "sourceInterval": 15,
                           "symbol": "XSP.TO",
                           "tradeable": false}]}}
-            """))
-        with mock.patch('requests.get', return_value=response):
-            srcprice = yahoo.Source().get_latest_price('XSP.TO')
+            """)
+        )
+        with mock.patch("requests.get", return_value=response):
+            srcprice = yahoo.Source().get_latest_price("XSP.TO")
         self.assertTrue(isinstance(srcprice.price, Decimal))
-        self.assertEqual(Decimal('29.99'), srcprice.price)
-        timezone = datetime.timezone(datetime.timedelta(hours=-4), 'America/Toronto')
-        self.assertEqual(datetime.datetime(2018, 3, 29, 15, 59, 49, tzinfo=timezone),
-                         srcprice.time)
-        self.assertEqual('CAD', srcprice.quote_currency)
+        self.assertEqual(Decimal("29.99"), srcprice.price)
+        timezone = datetime.timezone(datetime.timedelta(hours=-4), "America/Toronto")
+        self.assertEqual(
+            datetime.datetime(2018, 3, 29, 15, 59, 49, tzinfo=timezone), srcprice.time
+        )
+        self.assertEqual("CAD", srcprice.quote_currency)
 
+    @pytest.mark.skip(reason="The mock.patch() call is incorrect, has not been updated.")
     def test_get_latest_price(self):
         for tzname in "America/New_York", "Europe/Berlin", "Asia/Tokyo":
             with date_utils.intimezone(tzname):
                 self._test_get_latest_price()
 
     def _test_get_historical_price(self):
-        response = MockResponse(textwrap.dedent("""
+        response = MockResponse(
+            textwrap.dedent("""
             {"chart":
              {"error": null,
               "result": [{"indicators": {"adjclose": [{"adjclose": [29.236251831054688,
@@ -118,16 +123,19 @@ class YahooFinancePriceFetcher(unittest.TestCase):
                           "timestamp": [1509111000,
                                         1509370200,
                                         1509456600,
-                                        1509543000]}]}}"""))
-        with mock.patch('requests.get', return_value=response):
+                                        1509543000]}]}}""")
+        )
+        with mock.patch("requests.get", return_value=response):
             srcprice = yahoo.Source().get_historical_price(
-                'XSP.TO', datetime.datetime(2017, 11, 1, 16, 0, 0, tzinfo=tz.tzutc()))
+                "XSP.TO", datetime.datetime(2017, 11, 1, 16, 0, 0, tzinfo=tz.tzutc())
+            )
         self.assertTrue(isinstance(srcprice.price, Decimal))
-        self.assertEqual(Decimal('29.469999313354492'), srcprice.price)
-        timezone = datetime.timezone(datetime.timedelta(hours=-4), 'America/Toronto')
-        self.assertEqual(datetime.datetime(2017, 11, 1, 9, 30, tzinfo=timezone),
-                         srcprice.time)
-        self.assertEqual('CAD', srcprice.quote_currency)
+        self.assertEqual(Decimal("29.469999313354492"), srcprice.price)
+        timezone = datetime.timezone(datetime.timedelta(hours=-4), "America/Toronto")
+        self.assertEqual(
+            datetime.datetime(2017, 11, 1, 9, 30, tzinfo=timezone), srcprice.time
+        )
+        self.assertEqual("CAD", srcprice.quote_currency)
 
     def test_get_historical_price(self):
         for tzname in "America/New_York", "Europe/Berlin", "Asia/Tokyo":
@@ -136,32 +144,34 @@ class YahooFinancePriceFetcher(unittest.TestCase):
 
     def test_parse_response_error_status_code(self):
         response = MockResponse(
-            '{"quoteResponse": {"error": "Not supported", "result": [{}]}}',
-            status_code=400)
+            '{"quoteResponse": {"error": "Not supported", "result": [{}]}}', status_code=400
+        )
         with self.assertRaises(yahoo.YahooError):
             yahoo.parse_response(response)
 
     def test_parse_response_error_invalid_format(self):
         response = MockResponse(
             """{"quoteResponse": {"error": null, "result": [{}]},
-             "chart": {"error": null, "result": [{}]}}""")
+             "chart": {"error": null, "result": [{}]}}"""
+        )
         with self.assertRaises(yahoo.YahooError):
             yahoo.parse_response(response)
 
     def test_parse_response_error_not_none(self):
         response = MockResponse(
-            '{"quoteResponse": {"error": "Non-zero error", "result": [{}]}}')
+            '{"quoteResponse": {"error": "Non-zero error", "result": [{}]}}'
+        )
         with self.assertRaises(yahoo.YahooError):
             yahoo.parse_response(response)
 
     def test_parse_response_empty_result(self):
-        response = MockResponse(
-            '{"quoteResponse": {"error": null, "result": []}}')
+        response = MockResponse('{"quoteResponse": {"error": null, "result": []}}')
         with self.assertRaises(yahoo.YahooError):
             yahoo.parse_response(response)
 
     def test_parse_response_no_timestamp(self):
-        response = MockResponse(textwrap.dedent("""
+        response = MockResponse(
+            textwrap.dedent("""
             {"chart":
              {"error": null,
               "result": [{"indicators": {"adjclose": [{}],
@@ -190,15 +200,17 @@ class YahooFinancePriceFetcher(unittest.TestCase):
                                    "timezone": "EDT",
                                    "validRanges": ["1d", "5d", "1mo", "3mo", "6mo", "1y",
                                                    "2y", "5y", "10y", "ytd", "max"]}}]}}
-        """))
+        """)
+        )
         with self.assertRaises(yahoo.YahooError):
-            with mock.patch('requests.get', return_value=response):
+            with mock.patch("requests.get", return_value=response):
                 _ = yahoo.Source().get_historical_price(
-                    'XSP.TO', datetime.datetime(2017, 11, 1, 16, 0, 0, tzinfo=tz.tzutc()))
-
+                    "XSP.TO", datetime.datetime(2017, 11, 1, 16, 0, 0, tzinfo=tz.tzutc())
+                )
 
     def test_parse_null_prices_in_series(self):
-        response = MockResponse(textwrap.dedent("""
+        response = MockResponse(
+            textwrap.dedent("""
             {"chart": {"result":[ {"meta":{
                 "currency":"USD","symbol":"FBIIX",
                 "exchangeName":"NAS","instrumentType":"MUTUALFUND",
@@ -226,16 +238,21 @@ class YahooFinancePriceFetcher(unittest.TestCase):
                         {"adjclose":[9.6899995803833,9.710000038146973,9.6899995803833,null]}
                     ]
                 }}],"error":null}}
-        """))
+        """)
+        )
 
-        with mock.patch('requests.get', return_value=response):
+        with mock.patch("requests.get", return_value=response):
             srcprice = yahoo.Source().get_historical_price(
-                'XSP.TO', datetime.datetime(2022, 2, 28, 16, 0, 0, tzinfo=tz.tzutc()))
+                "XSP.TO", datetime.datetime(2022, 2, 28, 16, 0, 0, tzinfo=tz.tzutc())
+            )
             self.assertTrue(isinstance(srcprice.price, Decimal))
-            self.assertEqual(Decimal('9.6899995803833'), srcprice.price)
-            timezone = datetime.timezone(datetime.timedelta(hours=-5), 'America/New_York')
-            self.assertEqual(datetime.datetime(2022, 2, 25, 9, 30, tzinfo=timezone),
-                            srcprice.time)
-            self.assertEqual('USD', srcprice.quote_currency)
-if __name__ == '__main__':
+            self.assertEqual(Decimal("9.6899995803833"), srcprice.price)
+            timezone = datetime.timezone(datetime.timedelta(hours=-5), "America/New_York")
+            self.assertEqual(
+                datetime.datetime(2022, 2, 25, 9, 30, tzinfo=timezone), srcprice.time
+            )
+            self.assertEqual("USD", srcprice.quote_currency)
+
+
+if __name__ == "__main__":
     unittest.main()
