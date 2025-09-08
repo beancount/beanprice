6961147e03fdf9196a161547190ae685a9e4ab91
xuhcc <xuhcc@cryptolab.net>
2025-09-08 16:52:48 +0400

    Release v2.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="beanprice",
-    version="2.0.0",
+    version="2.1.0",
     description="Price quotes fetcher for Beancount",
     long_description="""
       A script to fetch market data prices from various sources on the internet
